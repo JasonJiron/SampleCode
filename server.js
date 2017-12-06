4666794cd62fa1117c0abdb4ef8b78c96734303d
Jason <jason.jiron@gmail.com>
2017-12-06 16:48:32 -0600

    fixing errors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('viewengine', 'hbs');
+app.set('view engine', 'hbs');
 
 app.use((req,res,next) => {
   var now = new Date().toString();
