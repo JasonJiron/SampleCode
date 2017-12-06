a78e28191886e2944a0785c65d17513655189920
Jason <jason.jiron@gmail.com>
2017-12-06 17:12:49 -0600

    fixed error

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
 const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
+
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unbale to append to server log');
-    }
-  })
+  fs.appendFile('server.log', log + '\n');
   next();
 });
+// app.use((req,res,next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
+//
+//   console.log(log);
+//   fs.appendFile('server.log', log + '\n', (err) => {
+//     if (err) {
+//       console.log('Unbale to append to server log');
+//     }
+//   })
+//   next();
+// });
+
 
 // app.use((req,res,next) => {
 //   res.render('maintenance.hbs')
@@ -49,7 +59,8 @@ app.get('/about', (req,res) => {
   });
 });
 
-app.get('./projects', (req,res) => {
+
+app.get('/projects', (req,res) => {
   res.render('projects.hbs', {
     pageTitle: 'Projects'
   });
@@ -58,9 +69,73 @@ app.get('./projects', (req,res) => {
 app.get('/bad', (req,res) => {
   res.send({
     errorMessage: 'Error while handeling request.'
-  })
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+//
+// const express = require('express');
+// const hbs = require('hbs');
+// const fs = require('fs');
+//
+// const port = process.env.PORT || 3000;
+// var app = express();
+//
+// hbs.registerPartials(__dirname + '/views/partials')
+// app.set('view engine', 'hbs');
+//
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
+//
+//   console.log(log);
+//   fs.appendFile('server.log', log + '\n');
+//   next();
+// });
+//
+// // app.use((req, res, next) => {
+// //   res.render('maintenance.hbs');
+// // });
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// hbs.registerHelper('getCurrentYear', () => {
+//   return new Date().getFullYear();
+// });
+//
+// hbs.registerHelper('screamIt', (text) => {
+//   return text.toUpperCase();
+// });
+//
+// app.get('/', (req, res) => {
+//   res.render('home.hbs', {
+//     pageTitle: 'Home Page',
+//     welcomeMessage: 'Welcome to my website'
+//   });
+// });
+//
+// app.get('/about', (req, res) => {
+//   res.render('about.hbs', {
+//     pageTitle: 'About Page'
+//   });
+// });
+//
+// app.get('/projects', (req, res) => {
+//   res.render('projects.hbs', {
+//     pageTitle: 'Projects'
+//   });
+// });
+//
+// // /bad - send back json with errorMessage
+// app.get('/bad', (req, res) => {
+//   res.send({
+//     errorMessage: 'Unable to handle request'
+//   });
+// });
+//
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}`);
+// });
